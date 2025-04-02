3d0599239a0267dbce59eff5ea9f155f718de888
Luiz <luizcruz02059345@gmail.com>
2025-04-01 23:00:41 -0300

    navegation feita

--- src/components/travel.vue
+++ src/components/travel.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="allPage">
+    <nav class="container">
+      <a href="#" id="logo">
+        <img src="../assets/Logo.svg" alt="Logomarca">
+      </a>
+      <ul id="primary-menu">
+        <img src="../assets/icones/MagnifyingGlass.svg" alt="">
+        <li><a href="#">Explorer</a></li>
+        <li><a href="#">Minhas viagens</a></li>
+        <li><a href="#">
+          <img src="../assets/Profile%20pic.png" alt="Imagem de Isabela Torres">
 
+        </a></li>
+      </ul>
+    </nav>
+    <header></header>
 
 
   </div>
@@ -12,4 +26,4 @@ export default {
 }
 </script>
 
-<style  src="./travel.css"/>
+<style src="./travel.css"/>
